01ce522348cab16068f1b7c776d5632209cd8310
Rafał <Rafa@wp.pl>
2019-04-12 03:43:06 +0200

    Updates

---
+++ precache-manifest.4a21b205b59101002c673483d750482b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "47afcf6bd2197cd6970164876906cc11",
+    "url": "/ToDoApp/static/media/bubble.47afcf6b.jpg"
+  },
+  {
+    "revision": "db14a749664a0df2ac75",
+    "url": "/ToDoApp/static/css/main.558a1d5c.chunk.css"
+  },
+  {
+    "revision": "fb1067834985d33aa2a1",
+    "url": "/ToDoApp/static/js/runtime~main.f2c45213.js"
+  },
+  {
+    "revision": "0c123523c8e42b3bd924",
+    "url": "/ToDoApp/static/js/2.d084b79b.chunk.js"
+  },
+  {
+    "revision": "db3bc4d3625805c05ee68ec566532c03",
+    "url": "/ToDoApp/static/media/lightbulb.db3bc4d3.jpg"
+  },
+  {
+    "revision": "db14a749664a0df2ac75",
+    "url": "/ToDoApp/static/js/main.c4a535ae.chunk.js"
+  },
+  {
+    "revision": "c53df244a82c95d02d97d373c39c7030",
+    "url": "/ToDoApp/static/media/bubblesSoap.c53df244.jpg"
+  },
+  {
+    "revision": "3f83c751064eef2b2dd5ba12aced905f",
+    "url": "/ToDoApp/static/media/thunderstorm.3f83c751.jpg"
+  },
+  {
+    "revision": "c82b34b98c27696faf26b137b8efc5c4",
+    "url": "/ToDoApp/static/media/Bridge.c82b34b9.jpg"
+  },
+  {
+    "revision": "1aacd0d320538f8c2dfff4c1f2ed774c",
+    "url": "/ToDoApp/static/media/sky.1aacd0d3.jpg"
+  },
+  {
+    "revision": "bd6813f0a213c45bc156fe73348ef1b3",
+    "url": "/ToDoApp/index.html"
+  }
+];
